ad1211fffbd4a46aecb4e0cd756e11ef3030195f
erikssm <eriks.sm@gmail.com>
2017-03-22 20:53:19 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+#include <chrono>
+#include <iostream>
+#include <boost/format.hpp>
+#include <string>
+
+namespace
+{
+using duration_microsec = std::chrono::duration< double, std::micro >;
+
+constexpr size_t n { 9999999 };
+std::vector<char> test( n );
+
+template < typename FN >
+inline duration_microsec  measure( FN& fn )
+{
+    auto ts1 = std::chrono::high_resolution_clock::now();
+    fn();
+    auto ts2 = std::chrono::high_resolution_clock::now();
+
+    return ( ts2 - ts1 );
+}
+
+void print_table_header()
+{
+    using namespace std;
+    cout << std::string ( 80, '-' ) << "\n";
+    cout << ( boost::format( "|%|3t|%s %|30t|%s %|60t|%s %|79t||\n" )
+                        % "std::for_each"
+                        % "simple for loop"
+                        % "difference"
+            ).str();
+    cout << std::string ( 80, '-' ) << "\n";
+}
+
+void run_test()
+{
+    using namespace std;
+
+    auto dummpy_fn = [](char c)
+    {
+    };
+
+    auto test1 = [&]() {
+        std::for_each( std::begin( test ), std::end( test ), dummpy_fn );
+    };
+    auto len1 = measure( test1 );
+
+    auto test2 = [&]() {
+        for ( auto it = test.begin(); it != test.end(); ++it )
+        {
+            dummpy_fn( *it );
+        }
+    };
+    auto len2 = measure( test2 );
+
+    cout << ( boost::format( "%|3t|%.1f microsec %|30t|%.1f microsec %|60t|%s %%\n" )
+                        % len1.count()
+                        % len2.count()
+                        % std::to_string( ( len2.count() / len1.count() - 1 ) * 100 )
+            ).str();
+}
+} // namespace
+
+int main(int argc, char **argv)
+{
+    using namespace std;
+
+    try
+    {
+        print_table_header();
+        for( auto i = 0; i < 11; ++i )
+        {
+            run_test();
+        }
+        cout << std::string ( 80, '-' ) << "\n";
+
+        return 0;
+    }
+    catch ( std::exception& e )
+    {
+        cerr << e.what() << endl;
+    }
+
+    return 1;
+}
